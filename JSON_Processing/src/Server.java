ae19efa0c0067bf0990f8677cd195da97016c737
Jeffreyhung <jeffreyhung@hotmail.com.tw>
2019-01-24 16:03:41 -0500

    2019.01.24

---
+++ JSON_Processing/src/Server.java
@@ -0,0 +1,73 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Server")
+public class Server extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Server() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.setCharacterEncoding("UTF-8");
+//		request.setCharacterEncoding("UTF-8");
+//		
+//		String requestUri = request.getRequestURI();
+//		
+//		response.setContentType("text/html");
+//		PrintWriter out = response.getWriter();
+//		
+//		out.println("<!DOCTYPE html>");
+//		out.println("<html>");
+//		out.println("<head>");
+//		out.println("<title>Hello world</title>");
+//		out.println("</head>");
+//		out.println("<body>");
+//		out.println("Hello World from frisrt servlet");
+//		
+//		out.println("<form action='" + requestUri + "' method='post'>");
+//		out.println("<input type='text' name='name' />");
+//		out.println("<input type='submit' value='submit' />");
+//		out.println("</form>");
+//		
+//		out.println("</body>");
+//		out.println("</html>");
+//		
+//		out.flush();
+//		out.close();
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		
+		String name = request.getParameter("name");
+	
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello world</title>");
+		out.println("</head>");
+		out.println("<body>");
+		
+		out.println("Hello: " + name);
+		
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.flush();
+		out.close();
+	}
+
+}
