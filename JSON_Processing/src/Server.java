c6e0a462c616fabe621cbec6fe48d3eaffea2942
Jeffreyhung <jeffreyhung@hotmail.com.tw>
2019-01-24 20:08:45 -0500

    Complete the basic frame

--- JSON_Processing/src/Server.java
+++ JSON_Processing/src/Server.java
@@ -16,58 +16,46 @@ public class Server extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		response.setCharacterEncoding("UTF-8");
-//		request.setCharacterEncoding("UTF-8");
-//		
-//		String requestUri = request.getRequestURI();
-//		
-//		response.setContentType("text/html");
-//		PrintWriter out = response.getWriter();
-//		
-//		out.println("<!DOCTYPE html>");
-//		out.println("<html>");
-//		out.println("<head>");
-//		out.println("<title>Hello world</title>");
-//		out.println("</head>");
-//		out.println("<body>");
-//		out.println("Hello World from frisrt servlet");
-//		
-//		out.println("<form action='" + requestUri + "' method='post'>");
-//		out.println("<input type='text' name='name' />");
-//		out.println("<input type='submit' value='submit' />");
-//		out.println("</form>");
-//		
-//		out.println("</body>");
-//		out.println("</html>");
-//		
-//		out.flush();
-//		out.close();
+		String input_text = request.getParameter("input_text").trim();
+
+	    response.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
+	    response.setCharacterEncoding("UTF-8"); // You want world domination, huh?
+	    response.getWriter().write(input_text);       // Write response body.
 	}
 
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setCharacterEncoding("UTF-8");
-		request.setCharacterEncoding("UTF-8");
-		
-		String name = request.getParameter("name");
-	
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Hello world</title>");
-		out.println("</head>");
-		out.println("<body>");
-		
-		out.println("Hello: " + name);
-		
-		out.println("</body>");
-		out.println("</html>");
-		
-		out.flush();
-		out.close();
-	}
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+////		response.setCharacterEncoding("UTF-8");
+////		request.setCharacterEncoding("UTF-8");
+////		
+////		String name = request.getParameter("name");
+////	
+////		response.setContentType("text/html");
+////		PrintWriter out = response.getWriter();
+////		
+////		out.println("<!DOCTYPE html>");
+////		out.println("<html>");
+////		out.println("<head>");
+////		out.println("<title>Hello world</title>");
+////		out.println("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">");
+////		out.println("<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>");
+////		out.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js\" integrity=\"sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49\" crossorigin=\"anonymous\"></script>");
+////		out.println("<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\" integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" crossorigin=\"anonymous\"></script>");
+////		out.println("</head>");
+////		out.println("<body>");
+////		out.println("<div class=\"card\" style=\"width: 18rem;\">");
+////		out.println("<div class=\"card-body\">");
+////		
+////		
+////		out.println("Hello: " + name);
+////		
+////		out.println("</div>");
+////		out.println("</div>");
+////		out.println("</body>");
+////		out.println("</html>");
+////		
+////		out.flush();
+////		out.close();
+//	}
 
 }
